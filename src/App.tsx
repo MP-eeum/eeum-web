ee8fe1b95ba27aa8994f360764ffd78789149edc
seorinn <seorin@ajou.ac.kr>
2024-10-15 04:33:58 +0900

    feat: 페이지 UI 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
 import LandingPage from "./pages/LandingPage";
+import NewsPage from "./pages/NewsPage";
+import WeatherPage from "./pages/WeatherPage";
+import DisasterPage from "./pages/DisasterPage";
+import EvacuationPage from "./pages/EvacuationPage";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="pb-20 App">
+    <div className="h-full pb-20 App">
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/weather" element={<WeatherPage />} />
+        <Route path="/disaster" element={<DisasterPage />} />
+        <Route path="/evacuation" element={<EvacuationPage />} />
       </Routes>
       <NavigationBar />
     </div>
