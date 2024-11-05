526e5a5689d2d0b7b490566bd1fd22501b928544
seorinn <seorin@ajou.ac.kr>
2024-11-05 22:34:08 +0900

    style: 헤더 로고 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import logo_eeum from "../../assets/images/logo_eeum.png";
+
 interface Props {
   title: string;
   button: any;
@@ -6,6 +8,7 @@ interface Props {
 export default function Header({ title, button }: Props) {
   return (
     <div className="fixed z-30 flex items-center justify-center h-12 bg-white w-96">
+      <img className="absolute left-7" alt="이음" src={logo_eeum} />
       <div className="text-lg">{title}</div>
       <div className="absolute right-3">{button}</div>
     </div>
