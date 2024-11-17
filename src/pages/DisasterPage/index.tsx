65f561a1c53a8d16b285fb91d217250585e79d23
seorinn <seorin@ajou.ac.kr>
2024-11-17 16:16:29 +0900

    feat: 병의원 정보 추가 및 버튼 기능 활성화

--- src/pages/DisasterPage/index.tsx
+++ src/pages/DisasterPage/index.tsx
@@ -3,22 +3,8 @@ import axios from "axios";
 import icn_redcross from "../../assets/icons/icn_redcross.svg";
 import icn_shelter from "../../assets/icons/icn_shelter.png";
 import icn_currentLoc from "../../assets/icons/icn_currentLoc.svg";
-import icn_marker from "../../assets/icons/icn_marker.svg";
 import Header from "../../components/Header";
 import CustomMarker from "./CustomMarker";
-
-const options = {
-  params: {
-    serviceKey: process.env.REACT_APP_SHELTER,
-    numOfRows: 100,
-    pageNo: 1,
-    returnType: "json",
-    startLot: "127.04",
-    endLot: "127.05",
-    startLat: "37.268",
-    endLat: "37.288",
-  },
-};
 
 export default function DisasterPage() {
   const mapRef = useRef<HTMLDivElement | null>(null);
@@ -27,14 +13,23 @@ export default function DisasterPage() {
   const [currentLoc, setCurrentLoc] = useState({ x: 0, y: 0 });
   const [shelters, setShelters] = useState([]);
   const [showShelters, setShowShelters] = useState(false);
+  const [showHospitals, setShowHospitals] = useState(false);
+  const [hospitals, setHospitals] = useState([]);
   const [showPath, setShowPath] = useState(false);
   const [seletedItem, setSelectedItem] = useState<any>();
+  const [loading, setLoading] = useState(false);
+  const [markers, setMarkers] = useState<any[]>([]);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position: any) =>
       setMap(position.coords.longitude, position.coords.latitude)
     );
   }, []);
+
+  useEffect(() => {
+    if (markers.length === 0) return;
+    setVisibleMarkers();
+  }, [markers, showHospitals, showShelters]);
 
   const setMap = (x: number, y: number) => {
     setCurrentLoc({
@@ -55,43 +50,90 @@ export default function DisasterPage() {
       map: map,
       icon: icn_currentLoc,
     });
-    getSheltersFunc().then((res) => addMarkers(res, map));
+    getSheltersFunc().then((s) =>
+      getHospitalsFunc().then((h) => addMarkers(s, h, map))
+    );
   };
 
   const getSheltersFunc = async () => {
+    const options = {
+      params: {
+        serviceKey: process.env.REACT_APP_SHELTER,
+        numOfRows: 100,
+        pageNo: 1,
+        returnType: "json",
+        startLot: "127.04",
+        endLot: "127.05",
+        startLat: "37.268",
+        endLat: "37.288",
+      },
+    };
+    setLoading(true);
     try {
       const res = await axios.get(`/api3/V2/api/DSSP-IF-10941`, options);
       setShelters(res.data.body);
       return res.data.body;
     } catch (e) {
       console.log(e);
+    } finally {
+      setLoading(false);
     }
   };
 
-  const addMarkers = async (shelterdata: any[], map: any) => {
+  const getHospitalsFunc = async () => {
+    const options = {
+      params: {
+        serviceKey: process.env.REACT_APP_HOSPITAL,
+        numOfRows: 1000,
+        pageNo: 43, //40. 42, 43, 44
+        returnType: "json",
+      },
+    };
+    setLoading(true);
+    try {
+      const res = await axios.get(`/api3/V2/api/DSSP-IF-00128`, options);
+      const filteredData = res.data.body.filter(
+        (item: any) =>
+          item.LAT > 37.268 &&
+          item.LAT < 37.288 &&
+          item.LOT_LAT > 127.04 &&
+          item.LOT_LAT < 127.05
+      );
+      setHospitals(filteredData);
+      return filteredData;
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const addMarkers = async (
+    shelterdata: any[],
+    hospitaldata: any[],
+    map: any
+  ) => {
     const array: {
       y: number;
       x: number;
       name: string;
       addr: string;
       type: string;
+      contentType: string;
     }[] = [];
     const promises = shelterdata.map((s: any) => {
       return new Promise<void>((resolve) => {
         naver.maps.Service.geocode(
           { query: s.RONA_DADDR },
           function (status, res) {
-            if (
-              status === naver.maps.Service.Status.OK
-              // &&
-              // res.v2.addresses.length > 0
-            )
+            if (status === naver.maps.Service.Status.OK)
               array.push({
                 y: s.LAT,
                 x: s.LOT,
                 name: s.REARE_NM,
                 addr: s.RONA_DADDR,
                 type: s.SHLT_SE_NM,
+                contentType: "shelter",
               });
             else console.log(s.RONA_DADDR);
             resolve();
@@ -99,37 +141,65 @@ export default function DisasterPage() {
         );
       });
     });
+    const promises2 = hospitaldata.map((h: any) => {
+      return new Promise<void>((resolve) => {
+        naver.maps.Service.geocode({ query: h.ADDR }, function (status, res) {
+          if (status === naver.maps.Service.Status.OK)
+            array.push({
+              y: h.LAT,
+              x: h.LOT_LAT,
+              name: h.INST_NM,
+              addr: h.ADDR,
+              type: h.HSPTL_CLSF_NM,
+              contentType: "hospital",
+            });
+          else console.log(h.ADDR);
+          resolve();
+        });
+      });
+    });
 
     await Promise.all(promises);
+    await Promise.all(promises2);
     drawMarkers(array, map);
   };
 
   const drawMarkers = (array: any[], map: any) => {
+    const arrayMarkers = [];
     for (var i = 0; i < array.length; i++) {
-      const { y, x, name, addr, type } = array[i];
+      const { y, x, name, addr, type, contentType } = array[i];
       const marker = new naver.maps.Marker({
         position: new naver.maps.LatLng(y, x),
         map: map,
-        title: name,
+        title: [contentType, name].join(" "),
         clickable: true,
         icon: {
           content: CustomMarker({ title: name }),
           anchor: new naver.maps.Point(40, 20),
         },
-        visible: true,
       });
-      marker.setTitle(name);
+      arrayMarkers.push(marker);
       naver.maps.Event.addListener(marker, "click", () => {
         setShowPath(true);
         setSelectedItem({ name: name, type: type, addr: addr, marker: marker });
       });
     }
+    setMarkers(arrayMarkers);
+  };
+
+  const setVisibleMarkers = () => {
+    for (var i = 0; i < markers.length; i++) {
+      if (markers[i].getTitle().startsWith("shelter"))
+        if (showShelters) markers[i].setVisible(true);
+        else markers[i].setVisible(false);
+      else if (markers[i].getTitle().startsWith("hospital"))
+        if (showHospitals) markers[i].setVisible(true);
+        else markers[i].setVisible(false);
+    }
   };
 
   const drawPath = async (marker: any) => {
     const mapOptions = {
-      // center: map.getCenter(),
-      // zoom: map.getZoom(),
       center: new naver.maps.LatLng(currentLoc.y, currentLoc.x),
       zoom: 16,
     };
@@ -158,7 +228,9 @@ export default function DisasterPage() {
         map: newMap,
         title: marker.getTitle(),
         icon: {
-          content: CustomMarker({ title: marker.getTitle() }),
+          content: CustomMarker({
+            title: marker.getTitle().split(" ").slice(1),
+          }),
           anchor: new naver.maps.Point(40, 20),
         },
       });
@@ -186,25 +258,28 @@ export default function DisasterPage() {
       map: map,
       icon: icn_currentLoc,
     });
-    addMarkers(shelters, map);
+    addMarkers(shelters, hospitals, map);
   };
 
   const getMarkerInfo = (item: any) => {
     const { name, type, addr, marker } = item;
     return (
       <div className="relative flex flex-col gap-3 bg-white px-6 py-8 rounded-t-3xl shadow-[0_0_15px_1px_rgba(0,0,0,0.08)]">
-        <div className="flex gap-3 items-center">
+        <div className="flex items-center gap-3">
           <div>{name}</div>
           <div className="text-[#bbbbbb] text-sm">{type}</div>
         </div>
         <div>{addr}</div>
         <div
-          className="text-white cursor-pointer py-3 w-full text-center bg-primary rounded-xl"
+          className="w-full py-3 text-center text-white cursor-pointer bg-primary rounded-xl"
           onClick={() => drawPath(marker)}
         >
           대피소 길찾기
         </div>
-        <div className="absolute top-4 right-6" onClick={() => closePath()}>
+        <div
+          className="absolute cursor-pointer top-4 right-6"
+          onClick={() => closePath()}
+        >
           x
         </div>
       </div>
@@ -216,17 +291,27 @@ export default function DisasterPage() {
       <div className="h-12">
         <Header title="긴급상황" button={null} />
       </div>
-      <div className="flex gap-3 absolute top-12 left-0 text-sm z-30 m-2 w-full">
-        <div className="flex gap-1 bg-white items-center px-2 w-fit py-1 rounded-xl shadow-lg cursor-pointer">
+      <div className="absolute left-0 z-30 flex w-full gap-3 m-2 text-sm top-12">
+        <div
+          className={`flex items-center gap-1 px-2 py-1 shadow-lg cursor-pointer w-fit rounded-xl ${showHospitals ? "bg-primary text-white" : "bg-white"}`}
+          onClick={() => {
+            if (!showPath) setShowHospitals(!showHospitals);
+          }}
+        >
           <img src={icn_redcross} />
           <div className="w-fit text-nowrap">응급진료</div>
         </div>
-        <div className="flex gap-1 bg-white items-center px-2 w-fit py-1 rounded-xl shadow-lg cursor-pointer">
+        <div
+          className={`flex items-center gap-1 px-2 py-1 shadow-lg cursor-pointer w-fit rounded-xl ${showShelters ? "bg-primary text-white" : "bg-white"}`}
+          onClick={() => {
+            if (!showPath) setShowShelters(!showShelters);
+          }}
+        >
           <img src={icn_shelter} />
           <p>대피소</p>
         </div>
       </div>
-      <div id="map" className="flex-1 w-full bg-lightgray z-20" ref={mapRef} />
+      <div id="map" className="z-20 flex-1 w-full bg-lightgray" ref={mapRef} />
       {showPath && seletedItem && (
         <div className="absolute bottom-0 z-30 w-full">
           {getMarkerInfo(seletedItem)}
