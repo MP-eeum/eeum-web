1da697d5e02aff5b56cfdab3c1b6560018daaf06
seorinn <seorin@ajou.ac.kr>
2024-11-20 16:26:38 +0900

    style: 이음 로고 이미지 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Header({ title, button }: Props) {
   return (
     <div className="fixed z-30 flex items-center justify-center h-12 bg-white w-96">
-      <img className="absolute left-7" alt="이음" src={logo_eeum} />
+      <img className="absolute w-8 left-7" alt="이음" src={logo_eeum} />
       <div className="text-lg">{title}</div>
       <div className="absolute right-3">{button}</div>
     </div>
