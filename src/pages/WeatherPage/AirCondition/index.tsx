3ee07f97ed696ca587b58b47c5e9fba87845055e
seorinn <seorin@ajou.ac.kr>
2024-11-20 21:26:02 +0900

    fix: 대기정체지수 오류 해결

--- src/pages/WeatherPage/AirCondition/index.tsx
+++ src/pages/WeatherPage/AirCondition/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import ProgressBar from "./Bar";
 import icn_info from "../../../assets/icons/icn_info.svg";
 
 interface Item {
@@ -25,19 +26,6 @@ export default function AirCondition({ data, today }: Props) {
     setState(data[0][`h${Math.floor(today.getHours() / 3) * 3}`]);
   }, []);
 
-  const getConditionBar = () => {
-    if (!state) return;
-    return (
-      <div className="relative w-full h-4 rounded-xl bg-lightgray">
-        <div
-          className={`absolute w-[${Number(state)}%] h-4 z-30 rounded-xl bg-primary`}
-        >
-          <div className="absolute right-0 w-4 h-4 border-2 border-white bg-none rounded-xl" />
-        </div>
-      </div>
-    );
-  };
-
   return (
     <div className="relative flex flex-col gap-3 px-5 py-4 border rounded-lg border-lightgray">
       <div className="flex flex-col font-semibold">
@@ -52,7 +40,7 @@ export default function AirCondition({ data, today }: Props) {
           />
         </div>
       </div>
-      {getConditionBar()}
+      <ProgressBar gauge={Number(state)} />
       {showInfo && (
         <div className="absolute z-50 px-12 top-12">
           <div className="flex flex-col w-64 p-4 text-sm bg-white border rounded-md shadow-lg border-lightgray">
