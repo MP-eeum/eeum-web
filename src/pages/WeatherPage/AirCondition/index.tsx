3d27f4f09e05c6f9a5e114d1df84f3b91db3c382
seorinn <seorin@ajou.ac.kr>
2024-11-20 19:01:42 +0900

    feat: 대기정체지수 설명 추가

--- src/pages/WeatherPage/AirCondition/index.tsx
+++ src/pages/WeatherPage/AirCondition/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import icn_info from "../../../assets/icons/icn_info.svg";
 
 interface Item {
   [key: string]: string;
@@ -10,6 +11,7 @@ interface Props {
 
 export default function AirCondition({ data, today }: Props) {
   const [state, setState] = useState<string>();
+  const [showInfo, setShowInfo] = useState(false);
 
   const getAirState = (input: number) => {
     if (input < 26) return "낮음";
@@ -19,6 +21,7 @@ export default function AirCondition({ data, today }: Props) {
   };
 
   useEffect(() => {
+    if (data.length === 0) return;
     setState(data[0][`h${Math.floor(today.getHours() / 3) * 3}`]);
   }, []);
 
@@ -36,11 +39,32 @@ export default function AirCondition({ data, today }: Props) {
   };
 
   return (
-    <div className="flex flex-col gap-3 px-5 py-4 border rounded-lg border-lightgray">
+    <div className="relative flex flex-col gap-3 px-5 py-4 border rounded-lg border-lightgray">
       <div className="flex flex-col font-semibold">
-        대기정체지수 {state} ({getAirState(Number(state))})
+        <div className="flex items-center gap-2">
+          <div>
+            대기정체지수 {state} ({getAirState(Number(state))})
+          </div>
+          <img
+            className="w-4 opacity-50 cursor-pointer"
+            src={icn_info}
+            onClick={() => setShowInfo(!showInfo)}
+          />
+        </div>
       </div>
       {getConditionBar()}
+      {showInfo && (
+        <div className="absolute z-50 px-12 top-12">
+          <div className="flex flex-col w-64 p-4 text-sm bg-white border rounded-md shadow-lg border-lightgray">
+            <div className="font-medium">대기정체지수</div>
+            <div className="text-xs font-normal text-textgray">
+              대기 상태에 의해 대기가 정체될 수 있는 가능성을 지수로 나타낸
+              것입니다. 미세먼지 지표와 함께 외부 활동을 결정할 때 활용할 수
+              있습니다.
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
